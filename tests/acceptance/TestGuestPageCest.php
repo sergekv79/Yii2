7e85016502efaef9cd1eccaf82e8787cf8bea4d9
sergekv79 <sergekv79@gmail.com>
2019-03-10 23:32:21 +0300

    Add acceptence test

---
+++ tests/acceptance/TestGuestPageCest.php
@@ -0,0 +1,13 @@
+<?php 
+
+class TestGuestPageCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+}
