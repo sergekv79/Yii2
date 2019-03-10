b016bdcf76fe0a0c2f665b32286b109a71cf8d70
sergekv79 <sergekv79@gmail.com>
2019-03-10 23:04:31 +0300

    Fix php code requirements

--- views/site/index.php
+++ views/site/index.php
@@ -1,1 +1,3 @@
 <h1>Ksv Tutorial!</h1>
+
+<h4>Welcome to the Yii2</h4>
--- web/index.php
+++ web/index.php
@@ -2,11 +2,9 @@
 // ini_set('error_reporting', E_ALL);
 // ini_set('display_errors', 1);
 
-define ('YII_DEBUG', true);
-
+define('YII_DEBUG', true);
 require __DIR__.'/../vendor/yiisoft/yii2/Yii.php';
 $config = require __DIR__.'/../config/web.php';
-
 $yii = new yii\web\Application($config);
 $yii->run();
 
